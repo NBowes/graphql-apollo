d7224deb3331cab87c7a9ca5b962a46381989915
Nathen Bowes <nathen.bowes@shopify.com>
2020-08-26 12:56:56 -0700

    add item and user types to grapqhl

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,17 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    # field name
+    field :items,
+      # result type
+      [Types::ItemType],
+      # null is required - must be true or false
+      null: false,
+      # human readable description
+      description: "Returns a list of items in the martian library"
+    
+    # resolver - must be the same as the field name
+    def items
+      Item.all
     end
   end
 end
